2c9a59de3a06c8c42dddde9f947b509d70554a98
czq1992 <404209646@qq.com>
2013-12-21 22:23:07 +0800

    修改jtree

--- UI3/src/Sync/MainInterface.java
+++ UI3/src/Sync/MainInterface.java
@@ -2,21 +2,38 @@ package Sync;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeWillExpandListener;
+import javax.swing.filechooser.FileSystemView;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.ExpandVetoException;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 
 import java.awt.Window.Type;
+
+import javax.swing.Icon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.JComboBox;
 import javax.swing.JSplitPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.Component;
+
 import javax.swing.JDesktopPane;
+
 import java.awt.Color;
 import java.awt.Insets;
 import java.io.File;
@@ -65,7 +82,7 @@ public class MainInterface extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JButton btnNewButton = new JButton("<html>����<br>����<html>");
+		JButton btnNewButton = new JButton("<html>返回<br>桌面<html>");
 		btnNewButton.setMargin(new Insets(0, 0, 0, 0));
 		btnNewButton.setBounds(0, 0, 50, 36);
 		panel.add(btnNewButton);
@@ -74,7 +91,7 @@ public class MainInterface extends JFrame {
 		comboBox.setEditable(true);
 		comboBox.setBounds(90, 0, 395, 21);
 		panel.add(comboBox);
-		
+		//comboBox.addItem("a");
 		JButton btnNewButton_2 = new JButton("");
 		btnNewButton_2.setBackground(Color.WHITE);
 		btnNewButton_2.setIcon(new ImageIcon(MainInterface.class.getResource("/Sync/Images/goback.jpg")));
@@ -86,7 +103,7 @@ public class MainInterface extends JFrame {
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
-		JButton btnn = new JButton("<html>�½�<br>����<html>");
+		JButton btnn = new JButton("<html>新建<br>任务<html>");
 		btnn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				NewTaskWindow.main(null);
@@ -96,7 +113,7 @@ public class MainInterface extends JFrame {
 		btnn.setBounds(0, 33, 52, 52);
 		panel_1.add(btnn);
 		
-		JButton btnNewButton_1 = new JButton("<html>����<br>�б�<html>");
+		JButton btnNewButton_1 = new JButton("<html>任务<br>列表<html>");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				TaskList.main(null);
@@ -106,18 +123,149 @@ public class MainInterface extends JFrame {
 		btnNewButton_1.setBounds(0, 95, 52, 52);
 		panel_1.add(btnNewButton_1);
 		
-		JButton button_1 = new JButton("<html>��ʷ<br>�汾<html>");
+		JButton button_1 = new JButton("<html>历史<br>版本<html>");
 		button_1.setMargin(new Insets(0, 0, 0, 0));
 		button_1.setBounds(0, 157, 52, 52);
 		panel_1.add(button_1);
 		
-		JScrollPane scrollPane = new JScrollPane();
+		//JScrollPane scrollPane = new JScrollPane();
+		//scrollPane.setBounds(77, 55, 413, 253);
+		//contentPane.add(scrollPane);
+		FileTree fileTree=new FileTree(comboBox);
+		FileTreeModel model=new FileTreeModel(new DefaultMutableTreeNode(new FileNode("root",null,null,true)));
+        fileTree.setModel(model);
+        fileTree.setCellRenderer(new FileTreeRenderer());
+        JScrollPane scrollPane = new JScrollPane(fileTree);
 		scrollPane.setBounds(77, 55, 413, 253);
 		contentPane.add(scrollPane);
-		
-		JTree tree = new JTree();
-		scrollPane.setRowHeaderView(tree);
-		
 
 	}
+}
+
+class FileTree extends JTree {
+    public TreePath mouseInPath;
+    protected FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+    public FileTree(final JComboBox jc){
+        setRootVisible(false);
+      //  addTreeExpansionListener(tel);
+        addTreeWillExpandListener(new TreeWillExpandListener() {
+            @Override
+            public void treeWillExpand(TreeExpansionEvent event) throws ExpandVetoException {
+                DefaultMutableTreeNode lastTreeNode =(DefaultMutableTreeNode) event.getPath().getLastPathComponent();
+                FileNode fileNode = (FileNode) lastTreeNode.getUserObject();
+                
+                System.out.println(fileNode.file.getAbsolutePath() );
+                jc.addItem(fileNode.file.getAbsolutePath());
+                if (!fileNode.isInit) {
+                    File[] files;
+                    if (fileNode.isDummyRoot) {
+                        files = fileSystemView.getRoots();
+                    } else {
+                        files = fileSystemView.getFiles(
+                                ((FileNode) lastTreeNode.getUserObject()).file,
+                                false);
+                    }
+                    for (int i = 0; i < files.length; i++) {
+                    	//files[i].
+                        FileNode childFileNode = new FileNode(
+                        		
+                                fileSystemView.getSystemDisplayName(files[i]),
+                                fileSystemView.getSystemIcon(files[i]), files[i],
+                                false);
+                        DefaultMutableTreeNode childTreeNode = new DefaultMutableTreeNode(childFileNode);
+                        lastTreeNode.add(childTreeNode);
+                    }
+                    //閫氱煡妯″瀷鑺傜偣鍙戠敓鍙樺寲
+                    DefaultTreeModel treeModel1 = (DefaultTreeModel) getModel();
+                    treeModel1.nodeStructureChanged(lastTreeNode);
+                }
+                //鏇存敼鏍囪瘑锛岄伩鍏嶉噸澶嶅姞杞�
+                fileNode.isInit = true;
+            }
+            @Override
+            public void treeWillCollapse(TreeExpansionEvent event) throws ExpandVetoException {
+
+            }
+        });
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                TreePath path=getPathForLocation(e.getX(), e.getY());
+
+                if(path!=null){
+                    if(mouseInPath!=null){
+                        Rectangle oldRect=getPathBounds(mouseInPath);
+                        mouseInPath=path;
+                        repaint(getPathBounds(path).union(oldRect));
+                    }else{
+                        mouseInPath=path;
+                        Rectangle bounds=getPathBounds(mouseInPath);
+                        repaint(bounds);
+                    }
+                }else if(mouseInPath!=null){
+                    Rectangle oldRect=getPathBounds(mouseInPath);
+                    mouseInPath=null;
+                    repaint(oldRect);
+                }
+            }
+        });
+    }
+}
+class FileNode{
+    public FileNode(String name,Icon icon,File file,boolean isDummyRoot){
+        this.name=name;this.icon=icon;this.file=file;this.isDummyRoot=isDummyRoot;
+    }
+    public boolean isInit;
+    public boolean isDummyRoot;
+    public String name;
+    public Icon icon;
+    public File file;
+}
+class FileTreeRenderer extends DefaultTreeCellRenderer{
+    public FileTreeRenderer(){
+    }
+    @Override
+    public Component getTreeCellRendererComponent(javax.swing.JTree tree,
+                                                  java.lang.Object value,
+                                                  boolean sel,
+                                                  boolean expanded,
+                                                  boolean leaf,
+                                                  int row,
+                                                  boolean hasFocus){
+
+        FileTree fileTree=(FileTree)tree;
+        JLabel label= (JLabel) super.getTreeCellRendererComponent(tree,value,sel,expanded,leaf,row,hasFocus);
+
+        DefaultMutableTreeNode node=(DefaultMutableTreeNode)value;
+        FileNode fileNode=(FileNode)node.getUserObject();
+        label.setText(fileNode.name);
+        label.setIcon(fileNode.icon);
+
+        label.setOpaque(false);
+        if(fileTree.mouseInPath!=null&&
+                fileTree.mouseInPath.getLastPathComponent().equals(value)){
+            label.setOpaque(true);
+            label.setBackground(new Color(255,0,0,90));
+        }
+        return label;
+    }
+}
+class FileTreeModel extends DefaultTreeModel {
+    public FileTreeModel(TreeNode root) {
+        super(root);
+         FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+            File[] files=fileSystemView.getRoots();
+        for (int i = 0; i < files.length; i++) {
+            FileNode childFileNode = new FileNode(fileSystemView.getSystemDisplayName(files[i]), fileSystemView.getSystemIcon(files[i]), files[i], false);
+            DefaultMutableTreeNode childTreeNode = new DefaultMutableTreeNode(childFileNode);
+            ((DefaultMutableTreeNode)root).add(childTreeNode);
+        }
+    }
+    @Override
+    public boolean isLeaf(Object node) {
+        DefaultMutableTreeNode treeNode=(DefaultMutableTreeNode)node;
+        FileNode fileNode=(FileNode)treeNode.getUserObject();
+        if(fileNode.isDummyRoot)return false;
+        return fileNode.file.isFile();
+    }
 }
